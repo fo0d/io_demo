c8f8258685b773eee2462b9b154721e9624f9e69
fo0d <sputnik.y3@gmail.com>
2021-05-03 16:30:03 -0600

    Adding files

---
+++ cmake-build-debug/CMakeFiles/io_demo.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/binary0x01/area51/y3libgen/hash.c" "/home/binary0x01/area51/io_demo/cmake-build-debug/CMakeFiles/io_demo.dir/home/binary0x01/area51/y3libgen/hash.c.o"
+  "/home/binary0x01/area51/y3libgen/io.c" "/home/binary0x01/area51/io_demo/cmake-build-debug/CMakeFiles/io_demo.dir/home/binary0x01/area51/y3libgen/io.c.o"
+  "/home/binary0x01/area51/y3libgen/list.c" "/home/binary0x01/area51/io_demo/cmake-build-debug/CMakeFiles/io_demo.dir/home/binary0x01/area51/y3libgen/list.c.o"
+  "/home/binary0x01/area51/y3libgen/primes.c" "/home/binary0x01/area51/io_demo/cmake-build-debug/CMakeFiles/io_demo.dir/home/binary0x01/area51/y3libgen/primes.c.o"
+  "/home/binary0x01/area51/y3libgen/str.c" "/home/binary0x01/area51/io_demo/cmake-build-debug/CMakeFiles/io_demo.dir/home/binary0x01/area51/y3libgen/str.c.o"
+  "/home/binary0x01/area51/io_demo/io_and_txt.c" "/home/binary0x01/area51/io_demo/cmake-build-debug/CMakeFiles/io_demo.dir/io_and_txt.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/io_demo.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "../build/io_demo"
+  "../build/io_demo.pdb"
+  "CMakeFiles/io_demo.dir/home/binary0x01/area51/y3libgen/hash.c.o"
+  "CMakeFiles/io_demo.dir/home/binary0x01/area51/y3libgen/io.c.o"
+  "CMakeFiles/io_demo.dir/home/binary0x01/area51/y3libgen/list.c.o"
+  "CMakeFiles/io_demo.dir/home/binary0x01/area51/y3libgen/primes.c.o"
+  "CMakeFiles/io_demo.dir/home/binary0x01/area51/y3libgen/str.c.o"
+  "CMakeFiles/io_demo.dir/io_and_txt.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/io_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
